42a2d0e40e74bafb86933f6fe846301e1d8aea69
Zineeb-t <zineb.tahirr@hotmail.com>
2026-07-15 12:34:29 +0000

    feat: add task service

---
+++ src/main/java/com/taskflow/taskflow/service/TaskService.java
@@ -0,0 +1,25 @@
+package com.taskflow.taskflow.service;
+
+import com.taskflow.taskflow.entity.Task;
+import com.taskflow.taskflow.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
